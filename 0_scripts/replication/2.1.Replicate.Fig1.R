f20d6a1389777cb5583536a2bb39d94f3eaadfe1
Sean Hambali <sean.hambali@gmail.com>
2024-02-21 10:35:11 -0500

    Modify font sizes for Fig 1

--- 0_scripts/replication/2.1.Replicate.Fig1.R
+++ 0_scripts/replication/2.1.Replicate.Fig1.R
@@ -12,7 +12,7 @@ library(ggplot2)
 library(ggpubr)
 
 # Set default theme
-default_theme <- theme_classic()
+default_theme <- theme_minimal()
 theme_set(default_theme)
 
 # ============================================================================ #
@@ -34,18 +34,25 @@ fig1_data <- read_csv(
 plot_fig1 <- fig1_data %>% 
   ggplot(aes(x = gs_rank, y = rank)) +
   geom_point() +
-  geom_text(aes(label = committee), size = 3, nudge_y = .5) +
+  geom_text(aes(label = committee), size = 5.5, nudge_y = .5) +
   geom_smooth(method = "lm") +
   stat_cor(
     aes(label = after_stat(r.label)),
-    method = "pearson"
+    method = "pearson",
+    size = 5.5
   ) +
-  coord_cartesian(xlim = c(20, 0), ylim = c(20, 0)) +
+  coord_cartesian(xlim = c(23, -2), ylim = c(20, 0)) +
   scale_y_reverse(breaks = c(1, 5, 10, 15, 20)) +
   scale_x_reverse(breaks = c(1, 5, 10, 15, 20)) +
   labs(
     x = "Groseclose-Stewart Ranking",
     y = "Coverage-based Ranking"
+  ) +
+  theme(
+    axis.title.x = element_text(size = 15), 
+    axis.title.y = element_text(size = 15),
+    axis.text.x = element_text(size = 14), 
+    axis.text.y = element_text(size = 14)
   )
 
 # ============================================================================ #
